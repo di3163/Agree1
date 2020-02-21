911dfccb643d7223019f92db5a2d1b56cc555031
Dmitry Ilyin <di31.63@gmail.com>
2020-02-21 18:58:17 +0300

    refactoring1

--- app/src/main/java/com/example/agree/MainActivity.java
+++ app/src/main/java/com/example/agree/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.agree;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout activitiMain;
     private ListView listMess;
     private TextView textViewInfo;
-    //List<String> messages = new ArrayList<>();
     private Button buttonGetMList;
     AsTask asTask;
     ArrayAdapter<String> adapter;
@@ -36,15 +30,12 @@ public class MainActivity extends AppCompatActivity {
         textViewInfo = findViewById(R.id.text_info);
         mailTask = new MailTask();
         displayAllMessages();
-        //listMess = (ListView)findViewById(R.id.listMess);
- //       adapter = new ArrayAdapter<String>(this, R.layout.str_mess, messages);
-//        listMess.setAdapter(adapter);
+
     }
 
     public void onButtonClic(View v){
         asTask = new AsTask();
         asTask.execute();
-        //displayAllMessages();
     }
 
     class AsTask extends AsyncTask<Void,  Void, String> {
@@ -64,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
         List<String> values = mailTask.getMessages();
         if(values.size() >0 ) {
             ListView listOfMessages = findViewById(R.id.listMess);
-            //String[] values = (String[]) mailTask.getMessages().toArray();
-
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.str_mess, R.id.textView, values);
             listOfMessages.setAdapter(adapter);
         }
